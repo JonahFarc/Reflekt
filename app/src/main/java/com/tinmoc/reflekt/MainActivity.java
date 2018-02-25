ad2a0a86b617d6e6b0330ccee05bec7d879939ca
JonahFarc <jonahfarc@gmail.com>
2018-02-24 21:34:57 -0600

    Added email-password authentication, registration, edited ui, added usernames, created basic users.

--- app/src/main/java/com/tinmoc/reflekt/MainActivity.java
+++ app/src/main/java/com/tinmoc/reflekt/MainActivity.java
@@ -1,5 +1,7 @@
 package com.tinmoc.reflekt;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -10,15 +12,26 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText editMessage;
     private DatabaseReference mDatabase;
     private RecyclerView mMessageList;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private FirebaseUser mCurrentUser;
+    private DatabaseReference mDatabaseUsers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,15 +49,44 @@ public class MainActivity extends AppCompatActivity {
         linearLayoutManager.setStackFromEnd(true);
         mMessageList.setLayoutManager(linearLayoutManager);
 
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if(firebaseAuth.getCurrentUser() == null)  {
+                    startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                }
+            }
+        };
     }
 
     public void sendButtonClicked(View view)
     {
+        mCurrentUser = mAuth.getCurrentUser();
+        mDatabaseUsers = FirebaseDatabase.getInstance().getReference().child("Users").child(mCurrentUser.getUid());
+
         final String messageValue = editMessage.getText().toString().trim();
         editMessage.setText("");
         if (!TextUtils.isEmpty(messageValue)) {
             final DatabaseReference newPost = mDatabase.push();
-            newPost.child("content").setValue(messageValue);
+            mDatabaseUsers.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    newPost.child("content").setValue(messageValue);
+                    newPost.child("username").setValue(dataSnapshot.child("Name").getValue()).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+
+                        }
+                    });
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+            mMessageList.scrollToPosition(mMessageList.getAdapter().getItemCount());
         }
     }
 
@@ -61,6 +103,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             protected void populateViewHolder(MessageViewHolder viewHolder, Message model, int position) {
                 viewHolder.setContent(model.getContent());
+                viewHolder.setUsername(model.getUsername());
             }
         };
         mMessageList.setAdapter(FBRA);
@@ -75,6 +118,10 @@ public class MainActivity extends AppCompatActivity {
         public void setContent(String content) {
             TextView message_content = (TextView) mView.findViewById(R.id.messageText);
             message_content.setText(content);
+        }
+        public void setUsername(String username) {
+            TextView username_content = (TextView) mView.findViewById(R.id.usernameText);
+            username_content.setText(username);
         }
     }
 }
--- app/src/main/java/com/tinmoc/reflekt/Message.java
+++ app/src/main/java/com/tinmoc/reflekt/Message.java
@@ -1,19 +1,22 @@
 package com.tinmoc.reflekt;
 
+import java.io.Console;
+
 /**
  * Created by M3800 on 2/24/2018.
  */
 
 public class Message {
-    private String content;
+    private String content,username;
 
     public Message()
     {
 
     }
-    public Message(String content)
+    public Message(String content, String username)
     {
         this.content = content;
+        this.username = username;
     }
 
     public String getContent() {
@@ -22,5 +25,13 @@ public class Message {
 
     public void setContent(String content) {
         this.content = content;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 }
